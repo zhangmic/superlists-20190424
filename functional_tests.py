f77fdf3253a18e081cdf1231e26eccb2d502523a
zhangmic <zhangmickey@163.com>
2019-05-05 16:54:19 +0800

    geckodriver.log

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,39 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
+	
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
+		
+	def tearDown(self):
+		self.browser.quit()
+		
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		
+		self.browser.get('http://localhost:8000')
+		
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('finish the test!')
 
-browser.get('http://localhost:8000')
+if __name__ == '__main__':
+	unittest.main()
+	
 
-assert 'Django' in browser.title
+
+
+
+
+
+
+
+
+
+
+
+# browser = webdriver.Firefox()
+
+# browser.get('http://localhost:8000')
+
+# assert 'Django' in browser.title
